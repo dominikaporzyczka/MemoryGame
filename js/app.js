197c03d68fc3d95654b26384c82e01a1fbba14c3
Dominika Porzyczka <dominikaporzyczka@gmail.com>
2018-03-03 19:24:07 +0100

    feat: Create basic set of icons

---
+++ js/app.js
@@ -0,0 +1,10 @@
+const basicSet = [
+    '<i class="far fa-gem"></i>',
+    '<i class="far fa-paper-plane"></i>',
+    '<i class="fas fa-anchor"></i>',
+    '<i class="fas fa-bomb"></i>',,
+    '<i class="fas fa-paw"></i>',
+    '<i class="fas fa-bolt"></i>',
+    '<i class="fas fa-cube"></i>',
+    '<i class="fas fa-leaf"></i>',
+];
