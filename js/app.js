00b1332a589ff1b0e1ef4612156c979e2161cb70
Dominika Porzyczka <dominikaporzyczka@gmail.com>
2018-03-03 19:54:11 +0100

    feat: Prepare HTML using the board

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+const container = document.querySelector('#container');
+
 const basicSet = [
     '<i class="far fa-gem"></i>',
     '<i class="far fa-paper-plane"></i>',
@@ -14,4 +16,17 @@ function prepareBoard() {
     shuffle(cards);
 
     return cards;
+}
+
+function prepareHTML(board, container) {
+    const listOfCard = document.createElement('ul');
+
+    for(let i = 0; i < board.length; i++) {
+        const newCard = document.createElement('li');
+        newCard.innerHTML = board[i];
+
+        listOfCard.appendChild(newCard);
+    }
+
+    container.appendChild(listOfCard);
 }
